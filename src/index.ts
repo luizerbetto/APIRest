50c56aa819b033cb70cc868316a6ea67c97c54fd
Luiz Felipe Z Masson <felipe_masson123@hotmail.com>
2022-10-15 21:17:34 -0300

    novas rotas pedidas

--- src/index.ts
+++ src/index.ts
@@ -6,19 +6,71 @@ const app = express();
 
 app.use(express.json());
 
+/**
+ * Get List o People
+ */
 app.get('/people', async (req, res) => {
     const peoples = await prisma.people.findMany();
     res.json(peoples);
 })
 
-app.get('/listevents', async (req, res) => {
-    const events = await prisma.event.findMany({
-      where: { published: true },
+/**
+ * Get one People by id
+ */
+app.get('/people/:id', async (req, res) => {
+    const { id } = req.params
+    const people = await prisma.people.findFirst({
+        where: { id: Number(id) },
     })
-    res.json(events)
+    res.json(people)
+})
+
+/**
+ * Add one People
+ */
+app.post(`/people`, async (req, res) => {
+    const { email, name, eventTile } = req.body
+    const result = await prisma.people.create({
+        data: { ...req.body },
+    })
+    res.json(result)
+})
+
+/**
+ * Update one People base on ID
+ */
+app.put('/people/publish/:id', async (req, res) => {
+    const { id } = req.params
+    const event = await prisma.people.update({
+        where: { id: Number(id) },
+        data: { ...req.body },
+    })
+    res.json(event)
 })
   
-app.get(`/event/:id`, async (req, res) => {
+/**
+ * Delete one People by ID
+ */
+app.delete(`/people/:id`, async (req, res) => {
+    const { id } = req.params
+    const people = await prisma.people.delete({
+      where: { id: Number(id) },
+    })
+    res.json(people)
+})
+
+/**
+ * Get List o Event
+ */
+ app.get('/event/list', async (req, res) => {
+    const events = await prisma.event.findMany();
+    res.json(events);
+})
+
+/**
+ * Get one Event by id
+ */
+app.get('/event/:id', async (req, res) => {
     const { id } = req.params
     const event = await prisma.event.findFirst({
         where: { id: Number(id) },
@@ -26,18 +78,9 @@ app.get(`/event/:id`, async (req, res) => {
     res.json(event)
 })
 
-app.post(`/people`, async (req, res) => {
-    const { email, name, eventTile } = req.body
-    const result = await prisma.people.create({
-        data: {
-            email,
-            name,
-            // event: { connect: { title: eventTile } },
-            },
-    })
-    res.json(result)
-})
-
+/**
+ * Add Event
+ */
 app.post(`/event`, async (req, res) => {
     const { title, content, authorEmail } = req.body
     const result = await prisma.event.create({
@@ -46,6 +89,9 @@ app.post(`/event`, async (req, res) => {
     res.json(result)
 })
 
+/**
+ * Publish one Event
+ */
 app.put('/event/publish/:id', async (req, res) => {
     const { id } = req.params
     const event = await prisma.event.update({
@@ -55,13 +101,33 @@ app.put('/event/publish/:id', async (req, res) => {
     res.json(event)
 })
   
-  app.delete(`/event/:id`, async (req, res) => {
+/**
+ * Delete one Event base on ID
+ */
+app.delete(`/event/:id`, async (req, res) => {
     const { id } = req.params
     const event = await prisma.event.delete({
       where: { id: Number(id) },
     })
     res.json(event)
-  })
+})
+
+/**
+ * List Events of a People by idPeople
+ */
+ app.get('/event/bypeople/:peopleId', async (req, res) => {
+    const { peopleId } = req.params
+    const events = await prisma.event.findMany({
+      where: { 
+        participants:  {
+            some: {
+                id: Number(peopleId)
+            }
+        }
+    },
+    })
+    res.json(events)
+})
 
 app.listen(3000, () => {
     console.log('listening on port 3000');
